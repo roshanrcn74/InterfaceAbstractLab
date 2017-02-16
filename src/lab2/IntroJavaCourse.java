e7b41e97b745d36653b7d9053ef7ff72fb5c4d88
roshann <roshann@192.168.2.7>
2017-02-16 14:39:27 -0600

    Added ITCourse interface and implements with concrete class

---
+++ src/lab2/IntroJavaCourse.java
@@ -0,0 +1,60 @@
+package lab2;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse implements ITCourse{
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    private String prerequisites;
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    @Override
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public double getCredits() {
+        return credits;
+    }
+
+    @Override
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    @Override
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    @Override
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    @Override
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    
+}
---
+++ src/lab2/IntroToProgrammingCourse.java
@@ -0,0 +1,72 @@
+package lab2;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroToProgrammingCourse implements ITCourse{
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+
+    public IntroToProgrammingCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    @Override
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    @Override
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public double getCredits() {
+        return credits;
+    }
+
+    @Override
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+
+    @Override
+    public String getCourseName() {
+        return courseName;
+    }
+
+    @Override
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+
+    @Override
+    public void setPrerequisites(String prerequisites) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
